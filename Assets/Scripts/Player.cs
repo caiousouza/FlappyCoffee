9748b2cdb3b1c42f41dd878d562e77cf8782087e
Caio Souza Queiroz <caiousouza113@gmail.com>
2022-09-14 19:51:58 -0400

    Animação do personagem ao voar

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,13 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Player : MonoBehaviour
 {
+
     private Vector3 direction;
     public float gravity = -9.8f;
     public float force = 5f;
 
+    private SpriteRenderer spriteRenderer;
+    public Sprite[] sprites;
+    private int spriteIndex;
 
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(AnimaSprite), 0.15f, 0.15f);
+    }
 
     private void Update()
     {
@@ -26,5 +39,16 @@ public class NewBehaviourScript : MonoBehaviour
 
         direction.y += gravity * Time.deltaTime;
         transform.position += direction * Time.deltaTime;
+    }
+
+    private void AnimaSprite()
+    {
+        spriteIndex++;
+
+        if(spriteIndex >= sprites.Length){
+            spriteIndex = 0;
+        }
+
+        spriteRenderer.sprite = sprites[spriteIndex];
     }
 }
