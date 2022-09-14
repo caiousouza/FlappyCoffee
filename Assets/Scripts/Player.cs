494f9c2d1e4d7cfb25ab6123eaebcc5cc2003b60
Caio Souza Queiroz <caiousouza113@gmail.com>
2022-09-14 18:56:59 -0400

    Script de movimentação e voo

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Vector3 direction;
+    public float gravity = -9.8f;
+    public float force = 5f;
 
-    // Update is called once per frame
-    void Update()
+    privete void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)){
+            direction = Vector3.up * force;
+        }
+
+        if(Input.touchCount > 0){
+            Touch touch = Input.GetTouch(0);
+
+            if(touch.phase == TouchPhase.Began){
+                direction = Vector3.up * force;
+            }
+        }
+
+        direction.y += gravity * Time.deltaTime;
+        transform.position += direction * Time.deltaTime;
     }
 }
